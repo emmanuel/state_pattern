4abeaf9cf192e417d928f1de2e9a4ce1272ae052
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-12-14 12:37:28 -0800

    Define StatePattern::State#state_name as a class method.

--- lib/state_pattern/state.rb
+++ lib/state_pattern/state.rb
@@ -1,6 +1,10 @@
 module StatePattern
   class State
 
+    def self.state_name
+      name
+    end
+
     def self.state_methods
       public_instance_methods - State.public_instance_methods
     end
@@ -14,7 +18,7 @@ module StatePattern
     end
 
     def state_name
-      self.class.name
+      self.class.state_name
     end
 
     def transition_to(state_class)
